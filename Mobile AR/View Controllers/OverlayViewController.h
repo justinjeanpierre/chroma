63e6552d97d183b4c803c76a6f0134f56c498efc
justinjeanpierre <justin.jeanpierre@gmail.com>
2017-01-12 18:51:43 -0500

    Add OverlayViewController to test OpenGL views.

---
+++ Mobile AR/View Controllers/OverlayViewController.h
@@ -0,0 +1,13 @@
+//
+//  OverlayViewController.h
+//  Mobile AR
+//
+//  Created by Justin Jean-Pierre on 2017-01-12.
+//  Copyright © 2017 Jean-Pierre Digital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OverlayViewController : UIView
+
+@end
---
+++ Mobile AR/View Controllers/OverlayViewController.m
@@ -0,0 +1,21 @@
+//
+//  OverlayViewController.m
+//  Mobile AR
+//
+//  Created by Justin Jean-Pierre on 2017-01-12.
+//  Copyright © 2017 Jean-Pierre Digital. All rights reserved.
+//
+
+#import "OverlayViewController.h"
+
+@implementation OverlayViewController
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
