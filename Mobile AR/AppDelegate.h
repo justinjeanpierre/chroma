43c538e0234b7eccd0532ab15779b47dc887df0d
justinjeanpierre <justin.jeanpierre@gmail.com>
2017-02-19 23:52:56 -0500

    Change main project language from Swift to Objective-C.

---
+++ Mobile AR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mobile AR
+//
+//  Created by Justin Jean-Pierre on 2017-02-19.
+//  Copyright © 2017 Jean-Pierre Digital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Mobile AR/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Mobile AR
+//
+//  Created by Justin Jean-Pierre on 2017-02-19.
+//  Copyright © 2017 Jean-Pierre Digital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
